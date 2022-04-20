c9415ca224058c37c741a5e8689f8b9dc75818f2
Yackis <101496534+Yackis@users.noreply.github.com>
2022-04-20 12:53:01 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lat
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Karyawan ucup = new Karyawan("1", "ucup", 3000000);
+            Karyawan eko = new Karyawan("2", "eko", 2000000);
+            Console.WriteLine("No   Nik/Nama        Gaji Bulanan");
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("1    {0}/{1}         {2}", ucup.NIK, ucup.Nama, ucup.GajiBulanan);
+            Console.WriteLine("2    {0}/{1}         {2}", eko.NIK, eko.Nama, eko.GajiBulanan);
+
+            Console.WriteLine();
+            Console.WriteLine("Naikkan Gaji Sebesar 10%");
+            Console.WriteLine();
+
+            ucup.NaikkanGaji(10);
+            eko.NaikkanGaji(10);
+
+            Console.WriteLine("No   Nik/Nama        Gaji Bulanan");
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("1    {0}/{1}         {2}", ucup.NIK, ucup.Nama, ucup.GajiBulanan);
+            Console.WriteLine("2    {0}/{1}         {2}", eko.NIK, eko.Nama, eko.GajiBulanan);
+            Console.ReadKey();
+        }
+    }
+}
